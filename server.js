762077cf4cd2019c93b9228458422ece3f746011
Carlos Mota <CarlosMota@protonmail.com>
2017-04-28 10:15:06 -0400

    Starter Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// grab our dependencies
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 4040;
+//configure our application
+
+//set routes
+app.get('/', (req, res) => {
+  res.send('Hello, I am the app');
+});
+
+//start our server
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
